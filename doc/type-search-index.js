b50d3f21a62073248b23d3464ec7eca67f658212
khaosmd <64732634+khaosmd@users.noreply.github.com>
2021-08-15 18:40:01 +0200

    Add files via upload

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Capstone1"},{"p":"<Unnamed>","l":"Invoice"},{"p":"<Unnamed>","l":"Person"},{"p":"<Unnamed>","l":"Project"}];updateSearchResults();
